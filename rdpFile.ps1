ee18daeaacd6695f33fcd6f725f3cdb466659cdb
cloudnowaustralia <39158554+cloudnowaustralia@users.noreply.github.com>
2018-05-10 22:54:49 +1000

    Create rdpFile.ps1

---
+++ rdpFile.ps1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+    Adds an RDP icon to users desktop
+.DESCRIPTION
+    A detailed description of the function or script. This keyword can be
+    used only once in each topic.
+.NOTES
+    File Name      : rdpFile.ps1
+    Author         : R.M. Winter (ross@cloudnowaustralia.com.au)
+    Prerequisite   : PowerShell V2 over Vista and upper.
+    Copyright 2018 - Ross Winter/Cloud Now Australia
+.LINK
+    Script posted over:
+    cloudnowaustralia.com.au
+.EXAMPLE
+    Example 1
+.EXAMPLE
+    Example 2
+#>
